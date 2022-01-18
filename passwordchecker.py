bc2b490beefcadf7c12118c6020b771b5bdcf85f
Santiago Passafiume <santiagopassafiume@protonmail.com>
2022-01-18 03:49:38 -0900

    implement main function

--- passwordchecker.py
+++ passwordchecker.py
@@ -25,3 +25,12 @@ def check_password(password):
     first_5_chars, remaining_chars = sha1_password[:5], sha1_password[5:]
     response = request_api_data(first_5_chars)
     return get_password_count(response, remaining_chars)
+
+
+def main(args):
+    for password in args:
+        count = check_password(password)
+        if count:
+            print(f'"{password}" was found {count} times.')
+        else:
+            print(f'"{password}" was NOT found.')
