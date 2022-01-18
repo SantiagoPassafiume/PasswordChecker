ca34466f0ab3034509321102892e0553be62e5a4
Santiago Passafiume <santiagopassafiume@protonmail.com>
2022-01-18 03:47:59 -0900

    implement get_password_count function

--- passwordchecker.py
+++ passwordchecker.py
@@ -1,5 +1,6 @@
 import requests
 import hashlib
+import sys
 
 
 def request_api_data(query_chars):
@@ -11,8 +12,16 @@ def request_api_data(query_chars):
     return response
 
 
+def get_password_count(hashes, hash_to_check):
+    hashes = (line.split(":") for line in hashes.text.splitlines())
+    for hash, count in hashes:
+        if hash == hash_to_check:
+            return count
+    return 0
+
+
 def check_password(password):
     sha1_password = hashlib.sha1(password.encode("utf-8")).hexdigest().upper()
     first_5_chars, remaining_chars = sha1_password[:5], sha1_password[5:]
     response = request_api_data(first_5_chars)
-    return response
+    return get_passwords(response, remaining_chars)
