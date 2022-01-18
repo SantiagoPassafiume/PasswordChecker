1653cada479ef55917969ac9ecabad9aab0f7372
Santiago Passafiume <santiagopassafiume@protonmail.com>
2022-01-18 03:48:29 -0900

    fix typo in check_password

--- passwordchecker.py
+++ passwordchecker.py
@@ -24,4 +24,4 @@ def check_password(password):
     sha1_password = hashlib.sha1(password.encode("utf-8")).hexdigest().upper()
     first_5_chars, remaining_chars = sha1_password[:5], sha1_password[5:]
     response = request_api_data(first_5_chars)
-    return get_passwords(response, remaining_chars)
+    return get_password_count(response, remaining_chars)
